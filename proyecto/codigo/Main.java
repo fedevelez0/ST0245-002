140ea7dcf10fb6e95b36b6f68d55e54ead0c1ae7
federico <73360688+fedevelez0@users.noreply.github.com>
2021-02-14 23:02:07 -0500

    Add files via upload

---
+++ proyecto/codigo/Main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String [] args) {
+        BufferedReader BufferedReader   = new BufferedReader ();
+        BufferedReader.BufferedReader ();
+    }
+}
+
