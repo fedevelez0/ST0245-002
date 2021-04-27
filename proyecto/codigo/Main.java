0b1ce542e4cae8a14085511130ab1b2240ce5c1d
federico <73360688+fedevelez0@users.noreply.github.com>
2021-04-27 17:39:09 -0500

    Delete Main.java

--- proyecto/codigo/Main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String [] args) {
-        BufferedReader BufferedReader   = new BufferedReader ();
-        BufferedReader.BufferedReader ();
-    }
-}
-
