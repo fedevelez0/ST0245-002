eabcbdc251bef3f5bab9db366db601d0ca64405d
federico <73360688+fedevelez0@users.noreply.github.com>
2021-05-11 17:16:12 -0500

    Create Node.java

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,12 @@
+package talleres;
+public class Node {
+
+    public Node left,rigth;
+    public int data;
+
+    public Node(int data) {
+        this.left = null;
+        this.rigth = null;
+        this.data = data;
+    }
+}
