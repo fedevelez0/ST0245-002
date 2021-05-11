c7d00fad777080e98753e89f93f0a619ec58a8be
federico <73360688+fedevelez0@users.noreply.github.com>
2021-05-11 17:00:25 -0500

    Update Taller8.java

--- talleres/taller08/Taller8.java
+++ talleres/taller08/Taller8.java
@@ -1,22 +1,8 @@
 package Taller__8;
-import java.util.stack;
+import java.util.*;
 
-/**
- * La clase Taller8 tiene la intención de
- * enfatizar en el uso de pilas y colas ya 
- * implementadas en java
- * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Queue.html"> Ver más acerca de colas<a/>
- * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html"> Ver más acerca de pilas <a/> 
- * @author Mauricio Toro, Andrés Páez 
- */
 public class Taller8 {
- 
-    /**
-    * @param string es una cadena de texto que viene de este modo 3 4 5 * + o de esta manera 2 3 * 5 +
-    * todo viene separado por espacios por lo que la funcion split.("") vendría muy bien para separarlos
-    * Se intenta hacer el calculo por medio del metodo polaco posfijo
-    * @return un entero resultado de la operación
-    */
+
     public static int polaca  (String string){
 	    String simbolos="+*/-";
 	    prefijo=String.split(" ");
@@ -24,20 +10,20 @@ public class Taller8 {
 	    
 	    for(int i=0; i<string.length;i++){
 		    if(simbolos.contains(""+string.charAt(i))){
-			    int primero=stack.pop();
-			    int segundo=stack.pop();
+			    int a=stack.pop();
+			    int b=stack.pop();
 			    char caracter=string.charAt(i);
 			    if(caracter="+"){
-				    stack.push(segundo+primero);
+				    stack.push(b+a);
 			    }
 			    if(caracter="-"){
-				    stack.push(segundo-primero);
+				    stack.push(b-a);
 			    }
 			    if(caracter="*"){
-				    stack.push(segundo*primero);
+				    stack.push(b*a);
 			    }
 			    if(caracter="/"){
-				    stack.push(segundo/primero);
+				    stack.push(b/a);
 			    }
 		    }else{
 			     stack.push(Integer.valueOf(string.charAt(i)));
@@ -46,30 +32,7 @@ public class Taller8 {
 	    return stack.pop;
     }
 
-   /**
-     * @param neveras es una estructura de datos para representar el almacen con las neveras
-     * @param solicitudes es una estructura de datos para representar las solicitudes
-     */
-    public static void asignarSolicitudes(Stack<Nevera> neveras, Queue<Solicitud> solicitudes){
-        while(solicitudes.peek() != null){
-            int cantidad = solicitudes.peek().numero;
-            System.out.println(solicitudes.poll().texto()+": ");
 
-            for(int i=0; i<cantidad; i++){
-                if(!neveras.isEmpty()){
-                    System.out.println("   -> "+ neveras.pop().texto());
-                }
-            }
-        }
-    }
-
-    /**
-     * @param stack es una pila ya implementada que se crea en el test
-     * Este método se encarga de poner la pila stack en orden inverso
-     * Nota: recuerde que la funcion pop() no solo expulsa la última 
-     * posición de una pila si no que tambien devuelve su valor.
-     * @return una pila que haga el inverso de stack
-     */
     public static Stack<Integer> inversa (Stack<Integer> stack){
         int n = stack.size();
         Stack<Integer> temp = new Stack<Integer>();
@@ -78,16 +41,10 @@ public class Taller8 {
         return temp;
     }
 
-    /**
-     * @param queue es una cola ya implementada que se crea en el test
-     * Este método se encarga de atender a personas.
-     * Nota: Se debe imprimir en consola a quién atiende y sacarlo de la cola
-     * existe una función "pull" que hace el trabajo más fácil
-     * 
-     */
-    public static void cola(Queue<String> que){
-        while(!que.isEmpty())    
-            System.out.println("Atendiendo a " + que.poll());
+
+    public static void cola(Queue<String> queue){
+        while(!queue.isEmpty())    
+            System.out.println("Atendiendo a " + queue.poll());
     }
 
 }
