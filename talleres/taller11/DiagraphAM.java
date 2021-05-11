378af30cf9c8169472a372d16549958bd5fba3da
federico <73360688+fedevelez0@users.noreply.github.com>
2021-05-11 17:25:09 -0500

    Create DiagraphAM.java

---
+++ talleres/taller11/DiagraphAM.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class DiagraphAM extends Graph {
+
+    int[][] mat;
+    public DiagraphAM(int size) {
+        super(size);
+        mat = new int[size][size];
+        for(int i = 0; i<size; i++){
+            mat[i][0]=i;
+            mat[0][i]=i;
+
+        }
+    }
+
+    public void addArc(int source, int destination, int weight) {
+        mat[source][destination] = weight;
+
+    }
+
+    public ArrayList<Integer> getSuccessors(int vertex) {
+            ArrayList<Integer> np = new ArrayList<>();
+            for(int i =1 ; i <size; i++){
+                if(mat[vertex][i]!=0){
+                    np.add(mat[0][i]);
+                }
+            }
+            return np;
+    }
+
+    public int getWeight(int source, int destination) {
+        return mat[source][destination];
+    }
+
+}
