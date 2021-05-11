7740dab3033681880496fefe309b4784aba7dd7c
federico <73360688+fedevelez0@users.noreply.github.com>
2021-05-11 17:01:59 -0500

    Update Taller8.java

--- talleres/taller08/Taller8.java
+++ talleres/taller08/Taller8.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class Taller8 {
 
     public static int polaca  (String string){
-	    String simbolos="+*/-";
+	    String s="+*/-";
 	    prefijo=String.split(" ");
 	    Stack<integer> stack=new Stack<>();
 	    
 	    for(int i=0; i<string.length;i++){
-		    if(simbolos.contains(""+string.charAt(i))){
+		    if(s.contains(""+string.charAt(i))){
 			    int a=stack.pop();
 			    int b=stack.pop();
 			    char caracter=string.charAt(i);
